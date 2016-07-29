f49886d1a848364d9409a3d7d5cb5d32d43729ab
Sparetire <s9ar371r3@gmail.com>
2016-07-29 15:41:11 +0800

    router

---
+++ src/webapp/components/app/script.js
@@ -0,0 +1,82 @@
+import co from 'co';
+import NProgress from 'nprogress/nprogress';
+import navBar from '../navBar/nav-bar';
+import logo from '../logo/logo';
+import info from '../info/info';
+import navList from '../navList/nav-list';
+import snsList from '../snsList/sns-list';
+import avatar from '../avatar/avatar';
+import mainContent from '../mainContent/main-content';
+import ArticleService from '../../services/ArticleService';
+import MarkdownParseService from '../../services/MarkdownParseService';
+import config from '../../config/webapp.conf';
+import APIConfig from '../../config/api.conf';
+import API from '../../../common/APIs';
+import RequestWrapper from '../../lib/RequestWrapper';
+import '../../style/app.scss';
+import 'nprogress/nprogress.css';
+
+/** init */
+let APIs = new API(APIConfig, RequestWrapper);
+ArticleService.apiConfig({
+	getArticles: APIs.getArticles
+});
+let articleService = ArticleService.getInstance();
+// MarkdownParseService.config({
+// 	renderer: new marked.Renderer(),
+// 	gfm: true,
+// 	tables: true,
+// 	breaks: false,
+// 	pedantic: false,
+// 	sanitize: true,
+// 	smartLists: true,
+// 	smartypants: false
+// });
+let markdownParser = MarkdownParseService.getInstance();
+let articles = [];
+co(function* () {
+	NProgress.start();
+	let remoteArticles = yield articleService.getArticles(1, config.pageLimit);
+	NProgress.inc(0.2);
+	for (let i in remoteArticles) {
+		remoteArticles[i].content = yield markdownParser.parse(remoteArticles[i]
+			.content);
+		articles.push(remoteArticles[i]);
+	}
+	NProgress.inc(0.3);
+	NProgress.done();
+});
+
+export default {
+	data: function () {
+		return {
+			currentLogo: config.defaultLogo,
+			navInfo: {
+				name: config.blogName,
+				intro: config.blogIntro
+			},
+			navItems: config.navList,
+			articles: articles
+		};
+	},
+	components: {
+		navBar,
+		logo,
+		info,
+		navList,
+		snsList,
+		avatar,
+		mainContent
+	},
+	events: {
+		navItemClick: function (msg) {
+			if (msg.content === 'About') {
+				this.currentLogo = 'avatar';
+				//route
+			} else {
+				this.currentLogo = 'logo';
+				//route
+			}
+		}
+	}
+};
--- src/webapp/main.js
+++ src/webapp/main.js
@@ -1,93 +1,23 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import $ from 'jquery';
-import navBar from './components/navBar/nav-bar';
-import logo from './components/logo/logo';
-import info from './components/info/info';
-import navList from './components/navList/nav-list';
-import snsList from './components/snsList/sns-list';
-import avatar from './components/avatar/avatar';
-import mainContent from './components/mainContent/main-content';
+import app from './components/app/app';
 import articleList from './components/articleList/article-list';
-import config from './config/webapp.conf';
-import ArticleService from './services/ArticleService';
-import MarkdownParseService from './services/MarkdownParseService';
-import APIConfig from './config/api.conf';
-import API from '../common/APIs';
-import RequestWrapper from './lib/RequestWrapper';
-import co from 'co';
-import NProgress from 'nprogress/nprogress';
-import './style/app.scss';
-import 'nprogress/nprogress.css';
 
-/** init */
-let APIs = new API(APIConfig, RequestWrapper);
-ArticleService.apiConfig({
-	getArticles: APIs.getArticles
+Vue.use(VueRouter);
+let router = new VueRouter();
+router.map({
+	'/': {
+		component: articleList
+	}
 });
-let articleService = ArticleService.getInstance();
-// MarkdownParseService.config({
-// 	renderer: new marked.Renderer(),
-// 	gfm: true,
-// 	tables: true,
-// 	breaks: false,
-// 	pedantic: false,
-// 	sanitize: true,
-// 	smartLists: true,
-// 	smartypants: false
-// });
-let markdownParser = MarkdownParseService.getInstance();
-
 
 $(() => {
-	let articles = [];
-	new Vue({
-		el: 'body',
-		data: function () {
-			return {
-				currentLogo: config.defaultLogo,
-				navInfo: {
-					name: config.blogName,
-					intro: config.blogIntro
-				},
-				navItems: config.navList,
-				articles: articles
-			};
-		},
-		components: {
-			navBar,
-			logo,
-			info,
-			navList,
-			snsList,
-			avatar,
-			mainContent,
-			articleList
-		},
-		events: {
-			navItemClick: function (msg) {
-				if (msg.content === 'About') {
-					this.currentLogo = 'avatar';
-					//route
-				} else {
-					this.currentLogo = 'logo';
-					//route
-				}
-			}
-		}
-	});
-	co(function* () {
-		NProgress.start();
-		let remoteArticles = yield articleService.getArticles(1, config.pageLimit);
-		// remoteArticles.forEach(function (item, index, array) {
-		// 	articles.push(item);
-		// });
-		NProgress.inc(0.2);
-		for (let i in remoteArticles) {
-			remoteArticles[i].content = yield markdownParser.parse(remoteArticles[i]
-				.content);
-			articles.push(remoteArticles[i]);
-		}
-		NProgress.inc(0.3);
-		NProgress.done();
-	});
+	router.start(app, 'app');
+	// new Vue({
+	// 	el: 'body',
+	// 	components: {
+	// 		app
+	// 	}
+	// });
 });
