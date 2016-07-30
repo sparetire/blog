3f2ebc5d74469276756f047d93d48db7e6f49520
Sparetire <s9ar371r3@gmail.com>
2016-07-30 15:03:55 +0800

    pages 20%

--- src/webapp/components/app/script.js
+++ src/webapp/components/app/script.js
@@ -10,6 +10,9 @@ import config from '../../config/webapp.conf';
 import APIConfig from '../../config/api.conf';
 import API from '../../../common/APIs';
 import RequestWrapper from '../../lib/RequestWrapper';
+import MarkdownParseService from '../../services/MarkdownParseService';
+import co from 'co';
+import NProgress from 'nprogress/nprogress';
 import '../../style/app.scss';
 import 'nprogress/nprogress.css';
 
@@ -33,6 +36,7 @@ ArticleService.apiConfig({
 // 等组件渲染好数据差不多也到了，而在组件中取数据则是等大部分组件渲染好后才发请求，
 // 稍慢一点，但在组件中取数据让路由更方便点
 
+
 export default {
 	data: function () {
 		return {
@@ -41,7 +45,8 @@ export default {
 				name: config.blogName,
 				intro: config.blogIntro
 			},
-			navItems: config.navList
+			navItems: config.navList,
+			articles: []
 		};
 	},
 	components: {
@@ -56,6 +61,23 @@ export default {
 	events: {
 		//切换应当和路由绑定而不是和点击事件绑定
 		onRouteChange: function (route) {
+			let articleService = ArticleService.getInstance();
+			let markdownParser = MarkdownParseService.getInstance();
+			// co会绑定this，所以在这里先把this.article取一下
+			let articles = this.articles;
+			co(function* () {
+				NProgress.start();
+				let remoteArticles = yield articleService.getArticles(1, config.pageLimit);
+				NProgress.inc(0.2);
+				for (let i in remoteArticles) {
+					remoteArticles[i].content = yield markdownParser.parse(remoteArticles[i]
+						.content);
+					articles.push(remoteArticles[i]);
+				}
+				NProgress.inc(0.3);
+				NProgress.done();
+			});
+
 			let path = route.path;
 			this.navItems.forEach(function (item, i, array) {
 				item.url === path ? item.active = true : item.active = false;
--- src/webapp/components/articleList/script.js
+++ src/webapp/components/articleList/script.js
@@ -1,9 +1,4 @@
 import articlePreview from '../articlePreview/article-preview';
-import MarkdownParseService from '../../services/MarkdownParseService';
-import ArticleService from '../../services/ArticleService';
-import config from '../../config/webapp.conf';
-import co from 'co';
-import NProgress from 'nprogress/nprogress';
 export default {
 	props: {
 		articles: {
@@ -18,23 +13,9 @@ export default {
 	},
 	route: {
 		data: function (transition) {
-			this.$dispatch('onRouteChange', this.$route);
-			let articleService = ArticleService.getInstance();
-			let markdownParser = MarkdownParseService.getInstance();
-			// co会绑定this，所以在这里先把this.article取一下
-			let articles = this.articles;
-			co(function* () {
-				NProgress.start();
-				let remoteArticles = yield articleService.getArticles(1, config.pageLimit);
-				NProgress.inc(0.2);
-				for (let i in remoteArticles) {
-					remoteArticles[i].content = yield markdownParser.parse(remoteArticles[i]
-						.content);
-					articles.push(remoteArticles[i]);
-				}
-				NProgress.inc(0.3);
-				NProgress.done();
-				transition.next();
+			let that = this;
+			return new Promise((resolve, reject) => {
+				that.$dispatch('onRouteChange', this.$route);
 			});
 		}
 	}
