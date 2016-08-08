d2100eb6ee7ea6539c78d346dd92d677adcc83b2
Sparetire <s9ar371r3@gmail.com>
2016-08-08 21:33:49 +0800

    重构一些组件

--- src/webapp/components/app/script.js
+++ src/webapp/components/app/script.js
@@ -6,67 +6,11 @@ import snsList from '../snsList/sns-list';
 import avatar from '../avatar/avatar';
 import mainContent from '../mainContent/main-content';
 import scrollTop from '../scrollTop/scroll-top';
-import ArticleService from '../../services/ArticleService';
 import config from '../../config/webapp.conf';
-import APIConfig from '../../config/api.conf';
-import API from '../../../common/APIs';
-import RequestWrapper from '../../lib/RequestWrapper';
-import MarkdownParseService from '../../services/MarkdownParseService';
-import router from '../../config/router';
-import routerMap from '../../config/routerMap';
-import co from 'co';
-import NProgress from 'nprogress/nprogress';
 import $ from 'jquery';
 import '../../style/app.scss';
-import 'nprogress/nprogress.css';
 
-/** init */
-let APIs = new API(APIConfig, RequestWrapper);
-ArticleService.apiConfig({
-	getArticles: APIs.getArticles
-});
-// MarkdownParseService.config({
-// 	renderer: new marked.Renderer(),
-// 	gfm: true,
-// 	tables: true,
-// 	breaks: false,
-// 	pedantic: false,
-// 	sanitize: true,
-// 	smartLists: true,
-// 	smartypants: false
-// });
-/** init */
 
-function loadArticleAside(ctx, route) {
-	let articleService = ArticleService.getInstance();
-	let markdownParser = MarkdownParseService.getInstance();
-	// co会绑定this，所以在这里先把this.article取一下
-	let articles = ctx.articles = [];
-	co(function* () {
-		NProgress.start();
-		// 高亮当前页
-		ctx.curPage = parseInt(route.params.page, 10);
-		if (ctx.curPage < 1 || ctx.curPage > ctx.total) {
-			ctx.curPage = 1;
-			router.go({
-				name: 'home',
-				params: {
-					page: 1
-				}
-			});
-			return;
-		}
-		let remoteArticles = yield articleService.getArticles(ctx.curPage, config.perPageLimit);
-		NProgress.inc(0.2);
-		for (let i in remoteArticles) {
-			remoteArticles[i].content = yield markdownParser.parse(remoteArticles[i]
-				.content);
-			articles.push(remoteArticles[i]);
-		}
-		NProgress.inc(0.3);
-		NProgress.done();
-	});
-}
 
 // 个人觉得在这里取文章列表比较好，因为此时还没渲染组件，先发起请求反正是异步，
 // 等组件渲染好数据差不多也到了，而在组件中取数据则是等大部分组件渲染好后才发请求，
@@ -94,11 +38,6 @@ export default {
 					intro: config.blogIntro
 				},
 				navItems: config.navList,
-				articles: [],
-				curPage: 2,
-				total: 43,
-				pageCount: config.pageCount,
-				urlTemplate: routerMap.home.path,
 				scrollTopShow: false
 			};
 		},
@@ -115,22 +54,22 @@ export default {
 		events: {
 			//切换应当和路由绑定而不是和点击事件绑定
 			onRouteChange(route) {
-				switch (route.name) {
-					case routerMap.home.name:
-						loadArticleAside(this, route);
-						break;
-					case routerMap.archives.name:
-						loadArticleAside(this, route);
-						break;
-					case routerMap.tags.name:
-						loadArticleAside(this, route);
-						break;
-					case routerMap.about.name:
-						loadArticleAside(this, route);
-						break;
-					default:
-						break;
-				}
+				// switch (route.name) {
+				// 	case routerMap.home.name:
+				// 		loadArticleAside(this, route);
+				// 		break;
+				// 	case routerMap.archives.name:
+				// 		loadArticleAside(this, route);
+				// 		break;
+				// 	case routerMap.tags.name:
+				// 		loadArticleAside(this, route);
+				// 		break;
+				// 	case routerMap.about.name:
+				// 		loadArticleAside(this, route);
+				// 		break;
+				// 	default:
+				// 		break;
+				// }
 
 				// 高亮当前导航栏
 				let path = route.path;
--- src/webapp/components/aside/script.js
+++ src/webapp/components/aside/script.js
@@ -1,32 +1,89 @@
 import articleList from '../articleList/article-list';
 import pagination from '../pagination/pagination';
+import config from '../../config/webapp.conf';
+import routerMap from '../../config/routerMap';
+import MarkdownParseService from '../../services/MarkdownParseService';
+import ArticleService from '../../services/ArticleService';
+import router from '../../config/router';
+import APIConfig from '../../config/api.conf';
+import API from '../../../common/APIs';
+import RequestWrapper from '../../lib/RequestWrapper';
+import co from 'co';
+import NProgress from 'nprogress/nprogress';
+import Promise from 'bluebird';
+import 'nprogress/nprogress.css';
+/** init */
+let APIs = new API(APIConfig, RequestWrapper);
+ArticleService.apiConfig({
+	getArticles: APIs.getArticles
+});
+// MarkdownParseService.config({
+// 	renderer: new marked.Renderer(),
+// 	gfm: true,
+// 	tables: true,
+// 	breaks: false,
+// 	pedantic: false,
+// 	sanitize: true,
+// 	smartLists: true,
+// 	smartypants: false
+// });
+/** init */
+
+
+
+
 export default {
-	props: {
-		articles: {
-			type: Array,
-			default: []
+	data() {
+			return {
+				articles: [],
+				curPage: 1,
+				pageCount: config.pageCount,
+				total: 1,
+				urlTemplate: routerMap.home.path
+			};
 		},
-		curPage: {
-			default: 1
+		components: {
+			articleList,
+			pagination
 		},
-		pageCount: {
-			default: 10
-		},
-		total: {
-			default: 0
-		},
-		urlTemplate: {
-			default: '/'
+		route: {
+			data(transition) {
+				let ctx = this;
+				this.$dispatch('onRouteChange', this.$route);
+				return new Promise(function (resolve, reject) {
+					let articleService = ArticleService.getInstance();
+					let markdownParser = MarkdownParseService.getInstance();
+					// co会绑定this，所以在这里先把this.article取一下
+					let articles = ctx.articles = [];
+					co(function* () {
+						NProgress.start();
+						ctx.curPage = parseInt(ctx.$route.params.page, 10);
+						// todo 取一下total的数据
+						if (ctx.curPage < 1 || ctx.curPage > ctx.total) {
+							ctx.curPage = 1;
+							router.go({
+								name: routerMap.home.name,
+								params: {
+									page: 1
+								}
+							});
+							resolve();
+							return;
+						}
+						let remoteArticles = yield articleService.getArticles(ctx.curPage,
+							config.perPageLimit);
+						NProgress.inc(0.2);
+						for (let i in remoteArticles) {
+							remoteArticles[i].content = yield markdownParser.parse(
+								remoteArticles[i]
+								.content);
+							articles.push(remoteArticles[i]);
+						}
+						resolve(articles);
+						NProgress.inc(0.3);
+						NProgress.done();
+					});
+				});
+			}
 		}
-	},
-	components: {
-		articleList,
-		pagination
-	},
-	route: {
-		data(transition) {
-			this.$dispatch('onRouteChange', this.$route);
-			transition.next();
-		}
-	}
 };
